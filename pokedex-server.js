f6427377f7606af116d10798fd8021cb7fec3f9e
dimaduc2 <dimaduc@gmail.com>
2021-03-19 09:28:44 +0700

    da tao pokedex-server

---
+++ pokedex-server.js
@@ -0,0 +1,17 @@
+// đây là Express - Web Server cho website pokedex
+const express = require('express');		    //phải mượn Express
+
+
+const app = express();
+
+
+
+
+
+const port = 5400;
+
+
+
+
+
+

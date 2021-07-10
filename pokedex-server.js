1990256fb3c70e9bf6f816cc955d20e7504f80b7
dimaduc2 <dimaduc@gmail.com>
2021-07-10 20:32:29 +0700

    xong phan them hay sua pokemon

--- pokedex-server.js
+++ pokedex-server.js
@@ -24,13 +24,17 @@ const connection = mongoose.connection; //  <=> giữa server và DB
 // sau đó, mở kết nối để 2 bên nói chuyện
 // hiện ra, thông báo là nói chuyện đc rồi
 connection.once('open', function() {
-console.log("Đã nói chuyện với MongoDB");
-})
+  console.log("Đã nói chuyện với MongoDB");  
+  
+ })
+
+
 
 // server bắt đầu nghe và đợi câu hỏi ở phòng PORT 500
 app.listen(PORT, function() {		          //chạy Web Server ở địa chỉ phòng này
-  console.log("đã bắt đầu server của pokemon đang đợi câu hỏi và ở phòng Port: " + PORT);
+  console.log("đã bắt đầu server của pokemon đang đợi câu hỏi và ở phòng Port: " + PORT); 
 });
+
 
 pokedexRoutes.route('/').get(function(req, res) {
   res.send('câu trả lời / của router');
@@ -42,34 +46,6 @@ app.get('/', (req, res) => {
   console.log('câu trả lời / của app');
 })
 
-pokedexRoutes.route('/cong/').get(function(req, res) {
-  let so1 = req.query.so1;
-  let so2 = req.query.so2;
-  res.json({'ketQua': Number(so1) +' + '+ Number(so2) +' = '+ (Number(so1) + Number(so2))})
-  // res.send('đã nhận câu hỏi cộng 2 số: '+so1+' và '+so2+' ra '+( Number(so1) + Number(so2) ));
-  console.log('đã nhận câu hỏi cộng 2 số: '+so1+' và '+so2+' ra '+( Number(so1) + Number(so2) ));
-})
-pokedexRoutes.route('/tru/').get(function(req, res) {
-  let so1 = req.query.so1;
-  let so2 = req.query.so2;
-  res.json({'ketQua': Number(so1) +' - '+ Number(so2) +' = '+ (Number(so1) - Number(so2))})
-  // res.send('đã nhận câu hỏi trừ 2 số: '+so1+' và '+so2+' ra '+( Number(so1) - Number(so2) ));
-  console.log('đã nhận câu hỏi trừ 2 số: '+so1+' và '+so2+' ra '+( Number(so1) - Number(so2) ));
-})
-pokedexRoutes.route('/nhan/').get(function(req, res) {
-  let so1 = req.query.so1;
-  let so2 = req.query.so2;
-  res.json({'ketQua': Number(so1) +' * '+ Number(so2) +' = '+ (Number(so1) * Number(so2))})
-  // res.send('đã nhận câu hỏi nhân 2 số: '+so1+' và '+so2+' ra '+( Number(so1) * Number(so2) ));
-  console.log('đã nhận câu hỏi nhân 2 số: '+so1+' và '+so2+' ra '+( Number(so1) * Number(so2) ));
-})
-pokedexRoutes.route('/chia/').get(function(req, res) {
-  let so1 = req.query.so1;
-  let so2 = req.query.so2;
-  res.json({'ketQua': Number(so1) +' / '+ Number(so2) +' = '+ (Number(so1) / Number(so2))})
-  // res.send('đã nhận câu hỏi chia 2 số: '+so1+' và '+so2+' ra '+( Number(so1) / Number(so2) ));
-  console.log('đã nhận câu hỏi chia 2 số: '+so1+' và '+so2+' ra '+( Number(so1) / Number(so2) ));
-})
 
 pokedexRoutes.route('/inNhieuLan/').get(function(req, res) {
   let ten = req.query.ten;
@@ -84,52 +60,67 @@ pokedexRoutes.route('/inNhieuLan/').get(function(req, res) {
   console.log(text);
 })
 
-// var ten = ['Pichu', 'Pikachu', 'Raichu'];
-
-pokedexRoutes.route('/timTen/').get(function(req, res) {
-  // let soArrayCuaTen = req.query.soArrayCuaTen;
-  // res.send(ten[soArrayCuaTen])
-  // if(0>soArrayCuaTen || soArrayCuaTen>=ten.length){
-  //   res.json({'ketQuaTenArray':'không có gì'})
-  // }
-  // else{
-  //   res.json({'ketQuaTenArray':ten[soArrayCuaTen]})
-  // }
-
-  pokedexModel.find({}, function(err, timPokedex){
-    let soArrayCuaTen = req.query.soArrayCuaTen;
-    if(0>soArrayCuaTen || soArrayCuaTen>=timPokedex.length){
-      // res.json({'ketQuaTenArray':'không có gì'})
-      console.log({'ketQuaTenArray':'không có gì'})
-    }
-    else{
-      res.json({'ketQuaTenArray':timPokedex[soArrayCuaTen]})
-      console.log({'ketQuaTenArray':timPokedex[soArrayCuaTen]})
-    }
-  
-  })
-  
-
-
-})
 pokedexRoutes.route('/timTatCaTen/').get(function(req, res) {
-  // res.json({'ketQuaTatCaTenArray':ten})
-  // res.send(ten[soArrayCuaTen])
-  pokedexModel.find({}, function(err, timPokedex){
+console.log(req.query.thuTu)
+  pokedexModel.find({}, function(err, timPokedexs){
+    if (err) {
+      console.log(err);
+      res.json('Không kết nối với MongoDB')
+    }
+    else {
+      console.log('đã tìm thấy ' + timPokedexs.length + ' Pokemon là: ' )
+      res.json(timPokedexs)
+    }
+  }).sort({[req.query.thuTu]:1, name:1})
+})
+
+pokedexRoutes.route('/xoa/').get(function(req, res) {
+  let id = req.query.idMuonXoa;
+  console.log(id)
+  pokedexModel.findByIdAndDelete(id, function (err) {
     if (err) {
       console.log(err);
     }
-    else {
-      console.log('đã tìm thấy ' + timPokedex.length + ' Pokemon là: ' + timPokedex)
-      res.json(timPokedex)
+    else{
+      console.log('đã xóa ' + id);
+      // pokedexModel.find({}, function(err, timPokedexs){
+      //   res.json(timPokedexs)
+      // }).sort({ [req.query.thuTu]:1, name:1})
     }
   })
 })
 
-pokedexRoutes.route('/themTen/').get(function(req, res) {
-  let tenMoi = req.query.tenMoi;
-  console.log(ten)
-  ten.push(tenMoi)
-  console.log(ten)  
+pokedexRoutes.route('/themPokemon/').post(function(req, res) {
+
+  console.log(req.body)
+  // res.json(req.body.name+'\n'+req.body.number)
+  
+
+  let pokedexMoi = new pokedexModel(req.body);
+  
+  pokedexMoi.save()
+            .then(pokedexMoi => {
+              console.log('đã cho thêm tên pokemon mới: ' + pokedexMoi.name);
+              res.json(req.body.name+'\n'+req.body.number)
+            })
+            .catch(err => {
+              console.log('Không lưu vào được Database')
+              res.json('err DB')
+            })
+
+
+
+
+
+  // let tenMoi = req.query.tenMoi;
+  // console.log(ten)
+
+
+  // ten.push(tenMoi)
+  // console.log(ten)
+
+
 })
+
+
 
--- pokemon.model.js
+++ pokemon.model.js
@@ -5,6 +5,18 @@ let pokedexModel = new Schema(
   {
     name: String,
     image: String,
+    number: Number,
+    type: [String],
+    hp: Number,
+    attack: Number,
+    defense: Number,
+    sp_atk: Number,
+    sp_def: Number,
+    speed: Number,
+    heightM: Number,
+    weightKG: Number,
+    evo_from: String,
+    evo_to: [String],
   },
   {collection: 'pokemon'}          //tên của collection trong MongoDB
 );
